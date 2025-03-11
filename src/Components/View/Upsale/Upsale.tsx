8af1d541a2daaaa48b03f89fdbe51199bd5f6a71
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2025-03-11 20:09:47 +0100

    redirection if upsale collection is not set for that item

--- src/Components/View/Upsale/Upsale.tsx
+++ src/Components/View/Upsale/Upsale.tsx
@@ -3,53 +3,72 @@ import { DataContext } from "../../../Contexts/DataContext/Datacontext";
 import DualUpsaleOption from "../../Reusables/UpsaleSteps/DualUpsaleOption/DualUpsaleOption";
 import MultiUpsaleOption from "../../Reusables/UpsaleSteps/MultiUpsaleOption/MultiUpsaleOption";
 import { UpsaleContext } from "../../../Contexts/UpsaleContext/UpsaleContext";
+import Modal from "../../Reusables/Modal";
+import { StepContext } from "../../../Contexts/StepContext/StepContext";
 
 const Upsale = () => {
   const { tmkData } = useContext(DataContext);
-  const {upsaleId} = useContext(UpsaleContext)
+  const { upsaleId } = useContext(UpsaleContext);
+  const { handleStepChange } = useContext(StepContext);
+
 
   // tuka ni treba upsale od ID
 
   const [upsaleStep, setUpsaleStep] = useState(0);
 
-  const upsaleData = tmkData.UpsaleColletions.find(upsale => upsale.Id === upsaleId)
-
-  const stepsLength = upsaleData?.UpsaleSteps?.length ?? 0
-  const upsaleStepData =
-  upsaleData!.UpsaleSteps[upsaleStep];
-
-  const handleUpsaleStepChange = (type: "increase" | "decrease") => {
-    if (type === "increase") {
-      setUpsaleStep((upsaleStep) => upsaleStep + 1);
-    } else {
-      setUpsaleStep((upsaleStep) => upsaleStep - 1);
-    }
-  };
-
-  console.log('Upsale Data',upsaleData)
-
-  return (
-    <section>
-      {upsaleStepData.Options.length === 2 ? (
-        <DualUpsaleOption
-          stepsLength={stepsLength}
-          upsaleStep={upsaleStep}
-          upsaleStepData={upsaleStepData}
-          handleUpsaleStepChange={handleUpsaleStepChange}
-          upsaleInfoData={upsaleData!}
-        />
-      ) : (
-        <MultiUpsaleOption
-          upsaleStepData={upsaleStepData}
-          handleUpsaleStepChange={handleUpsaleStepChange}
-          upsaleStep={upsaleStep}
-          stepsLength={stepsLength}
-          upsaleInfoData={upsaleData!}
-
-        />
-      )}
-    </section>
+  const upsaleData = tmkData.UpsaleColletions.find(
+    (upsale) => upsale.Id === upsaleId
   );
+
+  if (upsaleData) {
+    const stepsLength = upsaleData.UpsaleSteps?.length;
+    const upsaleStepData = upsaleData.UpsaleSteps[upsaleStep];
+
+    const handleUpsaleStepChange = (type: "increase" | "decrease") => {
+      if (type === "increase") {
+        setUpsaleStep((upsaleStep) => upsaleStep + 1);
+      } else {
+        setUpsaleStep((upsaleStep) => upsaleStep - 1);
+      }
+    };
+
+    console.log("Upsale Data", upsaleData);
+
+    return (
+      <section>
+        {upsaleStepData.Options.length === 2 ? (
+          <DualUpsaleOption
+            stepsLength={stepsLength}
+            upsaleStep={upsaleStep}
+            upsaleStepData={upsaleStepData}
+            handleUpsaleStepChange={handleUpsaleStepChange}
+            upsaleInfoData={upsaleData!}
+          />
+        ) : (
+          <MultiUpsaleOption
+            upsaleStepData={upsaleStepData}
+            handleUpsaleStepChange={handleUpsaleStepChange}
+            upsaleStep={upsaleStep}
+            stepsLength={stepsLength}
+            upsaleInfoData={upsaleData!}
+          />
+        )}
+      </section>
+    );
+  }
+
+  return  <Modal borderColor={"black"}>
+  <>
+    Upsale Data is missing 
+    <button
+      onClick={() => {
+        handleStepChange("order");
+      }}
+    >
+      Close
+    </button>
+  </>
+</Modal>;
 };
 
 export default Upsale;
